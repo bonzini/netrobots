e57d4fa7c27376fa43c7e715016ae438b6d8ff1d
Paolo Bonzini <bonzini@gnu.org>
2008-12-16 09:06:11 +0000

    initial stub for the client

---
+++ clients/robots.c
@@ -0,0 +1,41 @@
+#include "robots.h"
+
+int
+scan (int degree,int resolution)
+{
+}
+
+int
+cannon (int degree,int range)
+{
+}
+
+int
+drive (int degree,int speed)
+{
+}
+
+int
+damage()
+{
+}
+
+void
+cycle ()
+{
+}
+
+int
+speed()
+{
+}
+
+int
+loc_x()
+{
+}
+
+int
+loc_y()
+{
+}
