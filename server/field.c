e90300b5cad953b939323637cd4b6f8b29925192
Matthias Eberli <eberlim@lu.unisi.ch>
2008-12-18 14:32:45 +0000

    modified the shot_animation and added a first status bar

--- server/field.c
+++ server/field.c
@@ -32,24 +32,43 @@ degtorad(int degrees)
 }
 
 void
-shot_animation(cairo_t *cr)
+shot_animation(cairo_t *cr, double direction, struct cannon can)
 {
+	int time = can.timeToReload-150;/*reduce the reload time by half of it so it draws the
+										explosion and the flash for half the reload time*/
+	
+	if(can.timeToReload < 1)return;/*if the gun is loaded don't paint anything*/
+	
 	cairo_save (cr);
 	cairo_pattern_t *pat;
+
+	/*flash of the shot*/
+	cairo_rotate(cr, direction);
 	
 	pat = cairo_pattern_create_linear (128, 64,
                                      128, 128.0);
-	cairo_pattern_add_color_stop_rgba (pat, 0, 1, 0, 0, 1);
-	cairo_pattern_add_color_stop_rgba (pat, 1, 1, 1, 0, 1);
+	cairo_pattern_add_color_stop_rgba (pat, 0, 1, 0, 0, time/150);
+	cairo_pattern_add_color_stop_rgba (pat, 1, 1, 1, 0, time/150);
 	cairo_set_source (cr, pat);
 	
-
 	cairo_move_to (cr, 32, 50);
 	cairo_arc_negative (cr, 32, 32, 18, 90 * (M_PI/180.0), 0);
 	cairo_arc_negative (cr, 68, 32, 18, M_PI, 90 * (M_PI/180.0));
 	cairo_arc_negative (cr, 50, 50, 18, M_PI, 0);
 	cairo_fill (cr);
 	cairo_pattern_destroy (pat);
+	
+	/* explosion*/
+	cairo_arc (cr, can.x, can.y, 40, 0, 2*M_PI);
+	pat = cairo_pattern_create_radial (can.x, can.y,
+                                     10, can.x, can.y, 40);
+	cairo_pattern_add_color_stop_rgba (pat, 0, 1, 0, 0, time/150);
+	cairo_pattern_add_color_stop_rgba (pat, 0.3, 1, 0.5, 0, time/150);
+	cairo_pattern_add_color_stop_rgba (pat, 0.6, 1, 0.2, 0, time/150);
+	cairo_set_source (cr, pat);
+	cairo_fill (cr);
+	cairo_pattern_destroy (pat);
+	
 	cairo_restore(cr);
 
 }
@@ -101,6 +120,60 @@ draw_radar(cairo_t *cr, double direction)
 }
 
 /*
+ *draws the name and a health bar for every robot (maybe in the future also the reloading animation
+ */
+void
+draw_stats(cairo_t *cr, struct robot **all)
+{
+	int i;
+	int space = 50;
+	cairo_pattern_t *pat;
+
+	cairo_save (cr);
+	
+	for(i = 0; i < max_robots; i++){
+		
+		/* display the name of the robot*/
+		cairo_select_font_face (cr, "Sans", CAIRO_FONT_SLANT_NORMAL,
+                               CAIRO_FONT_WEIGHT_BOLD);
+		cairo_set_font_size (cr, 13.0);
+
+		cairo_move_to (cr, 10.0, 15.0+i*space);		
+		cairo_show_text (cr, all[i]->name);
+		
+		/* line with gradient from red to green*/
+		cairo_move_to (cr, 10, 25+i*space);
+		cairo_set_line_width (cr, 30);
+		cairo_line_to (cr, 110, 25+i*space);
+		pat = cairo_pattern_create_linear (0.0, 0.0,  100.0, 100.0);
+		cairo_pattern_add_color_stop_rgba (pat, 1, 1, 0, 0, 1);
+		cairo_pattern_add_color_stop_rgba (pat, 0, 0, 1, 0, 1);
+		cairo_set_source (cr, pat);
+		cairo_stroke (cr);
+		cairo_pattern_destroy (pat);
+		
+		/*black line on top of the colored one*/
+		cairo_move_to (cr, 10, 25+i*space);
+		cairo_set_line_width (cr, 24);
+		cairo_set_source_rgba (cr, 0, 0, 0, 0.5);
+		cairo_line_to (cr, 110 - all[i]->damage, 25+i*space);
+		cairo_stroke (cr);
+		
+		/*reloading animation goes here*/
+		
+		
+		/*separation line between the different robots*/
+		cairo_move_to (cr, 20, 45+i*space);
+		cairo_set_line_width (cr, 4);
+		cairo_set_source_rgb (cr, 0, 0, 0);
+		cairo_line_to (cr, 100, 45+i*space);
+		cairo_stroke (cr);
+		
+	}
+	cairo_restore (cr);
+}
+
+/*
  *draws a robot with a given size, using the various parameters(orientation, position,..)
  *from the robot struct
  */
@@ -141,7 +214,8 @@ draw_robot(cairo_t *cr, struct robot *myRobot, double size)
 	cairo_restore(cr);
 	draw_cannon(cr, degtorad(myRobot->cannon_degree));
 	draw_radar(cr, degtorad(myRobot->radar_degree));
-	shot_animation(cr);
+	shot_animation(cr, degtorad(myRobot->cannon_degree), myRobot->cannon[0]);
+	shot_animation(cr, degtorad(myRobot->cannon_degree), myRobot->cannon[1]);
 	cairo_restore(cr);
 }
 
@@ -168,7 +242,7 @@ do_map (cairo_t *cr, SDL_Event *event)
 	cairo_scale(cr, WIN_HEIGHT/1000.0, WIN_HEIGHT/1000.0);
 	for(i = 0; i < max_robots; i++){
 		draw_robot(cr, all_robots[i], 0.5);
-		}
+	}
 	cairo_restore(cr);	
 /* cairo_save (cairo_context);
   cairo_set_operator (cairo_context, CAIRO_OPERATOR_SOURCE);
